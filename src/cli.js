7b643f4d2269d2bc93cc210ac1b39847b1d7f0e9
BosEriko <bos.eriko@gmail.com>
2019-04-27 13:42:51 +0800

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -22,7 +22,37 @@ function parseArgumentsIntoOptions(rawArgs) {
  };
 }
 
+async function promptForMissingOptions(options) {
+ const defaultTemplate = 'JavaScript';
+ if (options.skipPrompts) {
+   return {
+     ...options,
+     template: options.template || defaultTemplate,
+   };
+ }
+
+ const questions = [];
+ if (!options.template) {
+   questions.push({
+     type: 'list',
+     name: 'template',
+     message: 'Please choose which project template to use',
+     choices: ['JavaScript', 'TypeScript'],
+     default: defaultTemplate,
+   });
+ }
+
+ if (!options.git) {
+   questions.push({
+     type: 'confirm',
+     name: 'git',
+     message: 'Initialize a git repository?',
+     default: false,
+   });
+ }
+
 export function cli(args) {
  let options = parseArgumentsIntoOptions(args);
+ options = await promptForMissingOptions(options);
  console.log(options);
 }
