fb6ae350fcf5e667665b635b9bbbb4d1eec2f873
BosEriko <bos.eriko@gmail.com>
2019-04-30 12:00:52 +0800

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import ImageFile from './image.jpg';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello Webpack'], ' ');
   element.classList.add('goodbye');
-
-  let imageTag = new Image();
-  imageTag.src = ImageFile;
-
-  element.appendChild(imageTag);
 
   return element;
 }
